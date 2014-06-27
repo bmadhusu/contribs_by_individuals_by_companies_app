0d8e52f61d5e1de3eaf9b26b662ddc56cbd5fd5b
B. Madhusudan <madhu_lists@yahoo.com>
2014-06-26 20:14:07 -0400

    Initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ContribsByIndividualsByCompaniesApp::Application.config.session_store :cookie_store, key: '_contribs_by_individuals_by_companies_app_session'
